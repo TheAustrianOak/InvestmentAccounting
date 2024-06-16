ec892dd0d28f83df74e72356bdcabaf2faa5743f
Maksym Leonenko <maksym.leonenko@diceus.com>
2024-06-16 18:11:16 +0300

    fix

--- WinFormTestApp/Form1.Designer.cs
+++ WinFormTestApp/Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace WinFormTestApp
+﻿using System.Drawing;
+
+namespace WinFormTestApp
 {
 	partial class Form1
 	{
@@ -49,45 +51,45 @@
 			this.groupBoxSellShares = new System.Windows.Forms.GroupBox();
 			this.groupBoxAddLot = new System.Windows.Forms.GroupBox();
 			this.groupBoxResults = new System.Windows.Forms.GroupBox();
-
 			this.groupBoxSellShares.SuspendLayout();
 			this.groupBoxAddLot.SuspendLayout();
 			this.groupBoxResults.SuspendLayout();
 			this.SuspendLayout();
-
-
+			// 
+			// label1
+			// 
 			this.label1.AutoSize = true;
 			this.label1.Location = new System.Drawing.Point(6, 22);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(66, 13);
+			this.label1.Size = new System.Drawing.Size(67, 13);
 			this.label1.TabIndex = 0;
 			this.label1.Text = "Shares Sold:";
-			
+			// 
 			// textBoxSharesSold
-			
+			// 
 			this.textBoxSharesSold.Location = new System.Drawing.Point(110, 19);
 			this.textBoxSharesSold.Name = "textBoxSharesSold";
 			this.textBoxSharesSold.Size = new System.Drawing.Size(100, 20);
 			this.textBoxSharesSold.TabIndex = 1;
-			
+			// 
 			// label2
-			
+			// 
 			this.label2.AutoSize = true;
 			this.label2.Location = new System.Drawing.Point(6, 48);
 			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(86, 13);
+			this.label2.Size = new System.Drawing.Size(84, 13);
 			this.label2.TabIndex = 2;
 			this.label2.Text = "Price Per Share:";
-			
+			// 
 			// textBoxPricePerShare
-			
+			// 
 			this.textBoxPricePerShare.Location = new System.Drawing.Point(110, 45);
 			this.textBoxPricePerShare.Name = "textBoxPricePerShare";
 			this.textBoxPricePerShare.Size = new System.Drawing.Size(100, 20);
 			this.textBoxPricePerShare.TabIndex = 3;
-			
+			// 
 			// buttonCalculate
-			
+			// 
 			this.buttonCalculate.Location = new System.Drawing.Point(9, 71);
 			this.buttonCalculate.Name = "buttonCalculate";
 			this.buttonCalculate.Size = new System.Drawing.Size(201, 23);
@@ -95,77 +97,77 @@
 			this.buttonCalculate.Text = "Calculate";
 			this.buttonCalculate.UseVisualStyleBackColor = true;
 			this.buttonCalculate.Click += new System.EventHandler(this.buttonCalculate_Click);
-		 
+			// 
 			// labelRemainingShares
-			
+			// 
 			this.labelRemainingShares.AutoSize = true;
 			this.labelRemainingShares.Location = new System.Drawing.Point(6, 22);
 			this.labelRemainingShares.Name = "labelRemainingShares";
-			this.labelRemainingShares.Size = new System.Drawing.Size(97, 13);
+			this.labelRemainingShares.Size = new System.Drawing.Size(96, 13);
 			this.labelRemainingShares.TabIndex = 5;
 			this.labelRemainingShares.Text = "Remaining Shares:";
-			 
+			// 
 			// labelCostBasisSold
-			
+			// 
 			this.labelCostBasisSold.AutoSize = true;
 			this.labelCostBasisSold.Location = new System.Drawing.Point(6, 48);
 			this.labelCostBasisSold.Name = "labelCostBasisSold";
-			this.labelCostBasisSold.Size = new System.Drawing.Size(88, 13);
+			this.labelCostBasisSold.Size = new System.Drawing.Size(83, 13);
 			this.labelCostBasisSold.TabIndex = 6;
 			this.labelCostBasisSold.Text = "Cost Basis Sold:";
-			 
+			// 
 			// labelCostBasisRemaining
-			
+			// 
 			this.labelCostBasisRemaining.AutoSize = true;
 			this.labelCostBasisRemaining.Location = new System.Drawing.Point(6, 74);
 			this.labelCostBasisRemaining.Name = "labelCostBasisRemaining";
-			this.labelCostBasisRemaining.Size = new System.Drawing.Size(120, 13);
+			this.labelCostBasisRemaining.Size = new System.Drawing.Size(112, 13);
 			this.labelCostBasisRemaining.TabIndex = 7;
 			this.labelCostBasisRemaining.Text = "Cost Basis Remaining:";
-			 
+			// 
 			// labelTotalProfit
-			
+			// 
 			this.labelTotalProfit.AutoSize = true;
 			this.labelTotalProfit.Location = new System.Drawing.Point(6, 100);
 			this.labelTotalProfit.Name = "labelTotalProfit";
-			this.labelTotalProfit.Size = new System.Drawing.Size(63, 13);
+			this.labelTotalProfit.Size = new System.Drawing.Size(61, 13);
 			this.labelTotalProfit.TabIndex = 8;
 			this.labelTotalProfit.Text = "Total Profit:";
-			
+			// 
 			// labelNewShares
-			
+			// 
 			this.labelNewShares.AutoSize = true;
 			this.labelNewShares.Location = new System.Drawing.Point(6, 22);
 			this.labelNewShares.Name = "labelNewShares";
-			this.labelNewShares.Size = new System.Drawing.Size(69, 13);
+			this.labelNewShares.Size = new System.Drawing.Size(68, 13);
 			this.labelNewShares.TabIndex = 9;
 			this.labelNewShares.Text = "New Shares:";
-			
+			// 
 			// textBoxNewShares
-			
+			// 
 			this.textBoxNewShares.Location = new System.Drawing.Point(110, 19);
 			this.textBoxNewShares.Name = "textBoxNewShares";
 			this.textBoxNewShares.Size = new System.Drawing.Size(100, 20);
 			this.textBoxNewShares.TabIndex = 10;
-			 
+			// 
 			// labelNewPrice
-			
+			// 
 			this.labelNewPrice.AutoSize = true;
 			this.labelNewPrice.Location = new System.Drawing.Point(6, 48);
 			this.labelNewPrice.Name = "labelNewPrice";
-			this.labelNewPrice.Size = new System.Drawing.Size(61, 13);
+			this.labelNewPrice.Size = new System.Drawing.Size(59, 13);
 			this.labelNewPrice.TabIndex = 11;
 			this.labelNewPrice.Text = "New Price:";
-			 
+			// 
 			// textBoxNewPrice
-			 
+			// 
 			this.textBoxNewPrice.Location = new System.Drawing.Point(110, 45);
 			this.textBoxNewPrice.Name = "textBoxNewPrice";
 			this.textBoxNewPrice.Size = new System.Drawing.Size(100, 20);
 			this.textBoxNewPrice.TabIndex = 12;
-			 
+			// 
 			// buttonAddLot
-			 
+			// 
 			this.buttonAddLot.Location = new System.Drawing.Point(9, 71);
 			this.buttonAddLot.Name = "buttonAddLot";
 			this.buttonAddLot.Size = new System.Drawing.Size(201, 23);
@@ -173,9 +175,9 @@
 			this.buttonAddLot.Text = "Add Lot";
 			this.buttonAddLot.UseVisualStyleBackColor = true;
 			this.buttonAddLot.Click += new System.EventHandler(this.buttonAddLot_Click);
-
+			// 
 			// groupBoxSellShares
-			
+			// 
 			this.groupBoxSellShares.Controls.Add(this.label1);
 			this.groupBoxSellShares.Controls.Add(this.textBoxSharesSold);
 			this.groupBoxSellShares.Controls.Add(this.label2);
@@ -187,9 +189,9 @@
 			this.groupBoxSellShares.TabIndex = 14;
 			this.groupBoxSellShares.TabStop = false;
 			this.groupBoxSellShares.Text = "Sell Shares";
-			
+			// 
 			// groupBoxAddLot
-			
+			// 
 			this.groupBoxAddLot.Controls.Add(this.labelNewShares);
 			this.groupBoxAddLot.Controls.Add(this.textBoxNewShares);
 			this.groupBoxAddLot.Controls.Add(this.labelNewPrice);
@@ -201,9 +203,9 @@
 			this.groupBoxAddLot.TabIndex = 15;
 			this.groupBoxAddLot.TabStop = false;
 			this.groupBoxAddLot.Text = "Add Lot";
-			 
+			// 
 			// groupBoxResults
-			
+			// 
 			this.groupBoxResults.Controls.Add(this.labelRemainingShares);
 			this.groupBoxResults.Controls.Add(this.labelCostBasisSold);
 			this.groupBoxResults.Controls.Add(this.labelCostBasisRemaining);
@@ -214,9 +216,10 @@
 			this.groupBoxResults.TabIndex = 16;
 			this.groupBoxResults.TabStop = false;
 			this.groupBoxResults.Text = "Results";
-			
+			this.groupBoxResults.Enter += new System.EventHandler(this.groupBoxResults_Enter);
+			// 
 			// Form1
-			
+			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(250, 370);
@@ -225,7 +228,7 @@
 			this.Controls.Add(this.groupBoxSellShares);
 			this.Name = "Form1";
 			this.Text = "Cost Accounting";
-
+			this.Load += new System.EventHandler(this.Form1_Load);
 			this.groupBoxSellShares.ResumeLayout(false);
 			this.groupBoxSellShares.PerformLayout();
 			this.groupBoxAddLot.ResumeLayout(false);
@@ -233,6 +236,7 @@
 			this.groupBoxResults.ResumeLayout(false);
 			this.groupBoxResults.PerformLayout();
 			this.ResumeLayout(false);
+
 		}
 	}
 }
--- WinFormTestApp/Form1.cs
+++ WinFormTestApp/Form1.cs
@@ -13,12 +13,13 @@ namespace WinFormTestApp
 {
 	public partial class Form1 : Form
 	{
-		private readonly IShareService shareManager;
+		private readonly IShareService shareService;
 
-		public Form1(IShareService shareManager)
+		public Form1(IShareService shareService)
 		{
-			this.shareManager = shareManager;
+			this.shareService = shareService;
 			InitializeComponent();
+			SetFormIcon();
 		}
 
 		private void buttonCalculate_Click(object sender, EventArgs e)
@@ -32,7 +33,7 @@ namespace WinFormTestApp
 					return;
 				}
 
-				int totalAvailableShares = shareManager.GetTotalAvailableShares();
+				int totalAvailableShares = shareService.GetTotalAvailableShares();
 				if (sharesSold > totalAvailableShares)
 				{
 					MessageBox.Show($"You cannot sell more shares than you have available. You have {totalAvailableShares} shares available.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Warning);
@@ -41,7 +42,7 @@ namespace WinFormTestApp
 
 				try
 				{
-					var result = shareManager.CalculateShares(sharesSold, pricePerShare);
+					var result = shareService.CalculateShares(sharesSold, pricePerShare);
 
 					labelRemainingShares.Text = $"Remaining Shares: {result.RemainingShares}";
 					labelCostBasisSold.Text = $"Cost Basis Sold: {result.CostBasisSold:C}";
@@ -64,7 +65,7 @@ namespace WinFormTestApp
 			if (int.TryParse(textBoxNewShares.Text, out int newShares) &&
 				decimal.TryParse(textBoxNewPrice.Text, out decimal newPrice))
 			{
-				shareManager.AddLot(newShares, newPrice);
+				shareService.AddLot(newShares, newPrice);
 				MessageBox.Show("New lot added successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
 				textBoxNewShares.Clear();
 				textBoxNewPrice.Clear();
@@ -73,6 +74,16 @@ namespace WinFormTestApp
 			{
 				MessageBox.Show("Please enter valid numbers for new shares and price.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Warning);
 			}
+		}
+
+		private void SetFormIcon()
+		{
+			Icon = new Icon("allvue.ico");
+		}
+
+		private void groupBoxResults_Enter(object sender, EventArgs e)
+		{
+
 		}
 
 		private void Form1_Load(object sender, EventArgs e)
